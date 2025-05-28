725c52c1e94b298ac187402b329c51226a4d1c00
KngSlr <145368839+SirGisa@users.noreply.github.com>
2025-05-28 11:49:38 +0300

    Add files via upload

---
+++ task2-4/task2-4/Model/ShapeModel.swift
@@ -0,0 +1,18 @@
+//
+//  ShapeModel.swift
+//  task2-2
+//
+//  Created by Shvarsman on 10.05.25.
+//
+
+import UIKit
+
+struct ShapeModel {
+    static let backgroundNames = [
+        "background1",
+        "background2",
+        "background3",
+        "background4",
+        "background5"
+    ]
+}
