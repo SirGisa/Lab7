66e57d1860f9bed4cd03c78b0718e31030e07400
KngSlr <145368839+SirGisa@users.noreply.github.com>
2025-05-28 11:17:35 +0300

    Add files via upload

---
+++ example1/example1/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  task1
+//
+//  Created by Shvarsman on 9.05.25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ example1/example1/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  task1
+//
+//  Created by Shvarsman on 9.05.25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
