fe56160b8c63223969fa868f65ce6865d92f1c5c
KngSlr <145368839+SirGisa@users.noreply.github.com>
2025-06-25 18:02:10 +0300

    add task

---
+++ example1/example1/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  task1
+//
+//  Created by georg on 22.05.25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ example1/example1/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  task1
+//
+//  Created by georg on 22.05.25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
